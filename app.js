c6edee0087bc7b584dbfb53c9f19ac7dcea9b572
shirodkarpushkar <pushkars@winjit.com>
2020-10-06 17:17:58 +0530

    cors added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import indexRouter from '@routes/index';
 import mainRoutes  from  '@routes/main';
-
+import cors from 'cors'
 var app = express();
 
 // view engine setup
@@ -16,6 +16,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
