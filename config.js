4c4d4d25badaeea9f370f66243d780e01d69abf6
shirodkarpushkar <pushkars@winjit.com>
2020-10-06 19:09:27 +0530

    added env variables

--- config.js
+++ config.js
@@ -1,15 +1,22 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+  path:
+    process.env.NODE_ENV === "production" ? ".env.production" : ".env.local",
+});
+
 module.exports = {
-  DBPORT: 3306,
-  DBHost: "localhost",
-  DBUser: "root",
-  DBPassword: "root@123",
-  DBName: "store",
-  tokenkey: "root@123",
-  emailVerificationLink: "http://localhost:8080/verify?token=",
-  resetPasswordLink: "http://localhost:8080/reset-password?token=",
-  tokenExpirationTime: 24 * 60 * 60,
-  SMTPemailAddress: "pushkars@winjit.com",
-  SMTPPassword: "waterplane@123",
-  SMTPSenderEmail: "pushkars@winjit.com",
-  supportEmail:'developers.store@smartstore.com',
+  DBPORT: process.env.DB_PORT,
+  DBHost: process.env.DB_HOST,
+  DBUser: process.env.DB_USER,
+  DBPassword: process.env.DB_PASSWORD,
+  DBName: process.env.DB_NAME,
+  tokenkey: process.env.TOKEN_KEY,
+  emailVerificationLink: process.env.EMAIL_VERIFICATION_LINK,
+  resetPasswordLink: process.env.RESET_PASSWORD_LINK,
+  tokenExpirationTime: process.env.TOKEN_EXPIRY_TIME,
+  SMTPemailAddress: process.env.SMTP_EMAIL,
+  SMTPPassword: process.env.SMTP_EMAIL_PASSWORD,
+  SMTPSenderEmail: process.env.SMTP_SENDER_EMAIL,
+  supportEmail: process.env.SUPPORT_EMAIL,
 };
