aecbc302a44879ee7c458ab0d4283e75b261ac49
shirodkarpushkar <pushkars@winjit.com>
2020-10-07 18:26:05 +0530

    get product by id

--- modules/products/controller.js
+++ modules/products/controller.js
@@ -6,13 +6,12 @@ import _ from "lodash";
 
 
 /**
- * API for email verification
+ * API for getting products
  * @param {*} req (registration details)
  * @param {*} res (json with success/failure)
  */
 async function getAllProducts(req, res) {
   try {
-    const email = res.locals.tokenInfo.email;
     const sqlQuery = "call getAllProducts()";
     const getDetails = await query(sqlQuery);
     return res.json({
@@ -32,7 +31,35 @@ async function getAllProducts(req, res) {
     });
   }
 }
+/**
+ * API for getting products
+ * @param {*} req (registration details)
+ * @param {*} res (json with success/failure)
+ */
+async function getProductById(req, res) {
+  try {
+    const productId = parseInt(req.params.id,10);
+    const sqlQuery = "call getProductById(?)";
+    const getDetails = await query(sqlQuery, [productId]);
+    return res.json({
+      status: {
+        code: statusCodes.success,
+        message: messages.success,
+      },
+      result: getDetails[0][0],
+    });
+  } catch (error) {
+    return res.json({
+      status: {
+        code: error.statusCode,
+        message: error.message,
+      },
+      result: JSON.stringify(error),
+    });
+  }
+}
 
 module.exports = {
   getAllProducts,
+  getProductById,
 };
--- modules/products/routes.js
+++ modules/products/routes.js
@@ -5,5 +5,6 @@ import { auth } from "@middlewares/index";
 const router = express.Router();
 
 router.get("/", auth.validateToken, api.getAllProducts);
+router.get("/:id", auth.validateToken, api.getProductById);
 
 module.exports = router;
