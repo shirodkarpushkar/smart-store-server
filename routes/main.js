7eac0335e3e381c9b52c5b532da4e48930dbc137
shirodkarpushkar <pushkars@winjit.com>
2020-10-07 18:26:29 +0530

    get all categories, get category by id

--- routes/main.js
+++ routes/main.js
@@ -2,10 +2,12 @@ import express from 'express'
 import customers from '@modules/customers/routes'
 import images from "@modules/images/routes";
 import products from "@modules/products/routes";
+import categories from "@modules/categories/routes";
 
 var app = express()
 app.use("/customers", customers);
 app.use("/images", images);
 app.use("/products", products);
+app.use("/categories", categories);
 
 module.exports = app
