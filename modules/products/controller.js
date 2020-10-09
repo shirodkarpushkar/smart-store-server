95cea4e56a32bd5ff40674183534d5540455ca38
shirodkarpushkar <pushkars@winjit.com>
2020-10-09 18:22:21 +0530

    get customer reviews for a particular product

--- modules/products/controller.js
+++ modules/products/controller.js
@@ -1,9 +1,6 @@
-
-import {  statusCodes, messages } from "@common/helpers";
+import { statusCodes, messages } from "@common/helpers";
 import query from "@database/index";
 import _ from "lodash";
-
-
 
 /**
  * API for getting products
@@ -38,7 +35,7 @@ async function getAllProducts(req, res) {
  */
 async function getProductById(req, res) {
   try {
-    const productId = parseInt(req.params.id,10);
+    const productId = parseInt(req.params.id, 10);
     const sqlQuery = "call getProductById(?)";
     const getDetails = await query(sqlQuery, [productId]);
     return res.json({
@@ -58,8 +55,64 @@ async function getProductById(req, res) {
     });
   }
 }
+/**
+ * API for getting reviews by productId
+ * @param {*} req (productId)
+ * @param {*} res (json with success/failure)
+ */
+async function getReviewsByProductId(req, res) {
+  try {
+    const productId = parseInt(req.params.id, 10);
+    const sqlQuery = `SELECT reviews.id,
+    title,
+    body,
+    rating,
+    product,
+    review_date,
+    customers.id as "customer_id",
+    customers.first_name as "customer_first_name",
+    customers.last_name as "customer_last_name",
+    images.destination as "customer_avatar"
+FROM reviews
+LEFT JOIN customers ON ( reviews.customer = customers.id )
+LEFT JOIN images ON ( customers.avatar = images.id )
+where product = ?`;
+    const getDetails = await query(sqlQuery, [productId]);
+    let response = getDetails.map((el) => {
+      return {
+        id: el.id,
+        title: el.title,
+        body: el.body,
+        rating: el.rating,
+        product: el.product,
+        customer: {
+          id: el.customer_id,
+          name: el.customer_first_name + " " + el.customer_last_name,
+          avatar : el.customer_avatar
+        },
+        createdAt: el.review_date,
+      };
+    })
+    return res.json({
+      status: {
+        code: statusCodes.success,
+        message: messages.success,
+      },
+      result: response,
+    });
+  } catch (error) {
+    return res.json({
+      status: {
+        code: error.statusCode,
+        message: error.message,
+      },
+      result: JSON.stringify(error),
+    });
+  }
+}
 
 module.exports = {
   getAllProducts,
   getProductById,
+  getReviewsByProductId,
 };
--- modules/products/routes.js
+++ modules/products/routes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get("/", auth.validateToken, api.getAllProducts);
 router.get("/:id", auth.validateToken, api.getProductById);
+router.get("/:id/reviews", auth.validateToken, api.getReviewsByProductId);
 
 module.exports = router;
